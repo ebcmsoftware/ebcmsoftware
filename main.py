295eff44b7351c8a254e28c9518e785e9f9fc7e4
Eric Bailey <popcorncolonel@gmail.com>
2015-06-28 18:53:23 -0700

    dontae

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import webapp2
 import os
 from google.appengine.ext.webapp import template
-
 import jinja2
-
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+class Donate(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('donate.html')
+        self.response.write(template.render({}))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -17,5 +19,6 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write(template.render({}))
 
 app = webapp2.WSGIApplication([
+    ('/donate', Donate),
     ('/', MainHandler)
 ], debug=True)
